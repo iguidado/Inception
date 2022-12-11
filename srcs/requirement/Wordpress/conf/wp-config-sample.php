dcd919c06ba7eba4bbf3b4532c38743b2a333f77
iguidado <iguidado@inception.iguidado.42.fr>
2022-12-11 05:17:45 +0100

    stack is functionnal

--- srcs/requirement/Wordpress/conf/wp-config-sample.php
+++ srcs/requirement/Wordpress/conf/wp-config-sample.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME)'));
+define( 'DB_NAME', 'votre_nom_de_bdd' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', getenv('WORDPRESS_DB_USER)'));
+define( 'DB_USER', 'votre_utilisateur_de_bdd' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASS)'));
+define( 'DB_PASSWORD', 'votre_mdp_de_bdd' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', getenv('WORDPRESS_DB_HOST)'));
+define( 'DB_HOST', 'localhost' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -42,6 +42,26 @@ define( 'DB_CHARSET', 'utf8' );
  */
 define( 'DB_COLLATE', '' );
 
+/**#@+
+ * Clés uniques d’authentification et salage.
+ *
+ * Remplacez les valeurs par défaut par des phrases uniques !
+ * Vous pouvez générer des phrases aléatoires en utilisant
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clés secrètes de WordPress.org}.
+ * Vous pouvez modifier ces phrases à n’importe quel moment, afin d’invalider tous les cookies existants.
+ * Cela forcera également tous les utilisateurs à se reconnecter.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_KEY',  'mettez une phrase unique ici' );
+define( 'LOGGED_IN_KEY',    'mettez une phrase unique ici' );
+define( 'NONCE_KEY',        'mettez une phrase unique ici' );
+define( 'AUTH_SALT',        'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_SALT', 'mettez une phrase unique ici' );
+define( 'LOGGED_IN_SALT',   'mettez une phrase unique ici' );
+define( 'NONCE_SALT',       'mettez une phrase unique ici' );
+/**#@-*/
 
 /**
  * Préfixe de base de données pour les tables de WordPress.
@@ -50,7 +70,7 @@ define( 'DB_COLLATE', '' );
  * si vous leur donnez chacune un préfixe unique.
  * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
  */
-$table_prefix = getenv('WORDPRESS_TABLE_PREFIX');
+$table_prefix = 'wp_';
 
 /**
  * Pour les développeurs : le mode déboguage de WordPress.
@@ -76,22 +96,3 @@ if ( ! defined( 'ABSPATH' ) )
 
 /** Réglage des variables de WordPress et de ses fichiers inclus. */
 require_once( ABSPATH . 'wp-settings.php' );
-
-/**#@+
- * Clés uniques d’authentification et salage.
- *
- * Les Clés sont remplacer par le fichier salt.php grace a l'include si dessous
- * 
- * @since 2.6.0
-*	
-*	define( 'AUTH_KEY',         'mettez une phrase unique ici' );
-*	define( 'SECURE_AUTH_KEY',  'mettez une phrase unique ici' );
-*	define( 'LOGGED_IN_KEY',    'mettez une phrase unique ici' );
-*	define( 'NONCE_KEY',        'mettez une phrase unique ici' );
-*	define( 'AUTH_SALT',        'mettez une phrase unique ici' );
-*	define( 'SECURE_AUTH_SALT', 'mettez une phrase unique ici' );
-*	define( 'LOGGED_IN_SALT',   'mettez une phrase unique ici' );
-*	define( 'NONCE_SALT',       'mettez une phrase unique ici' );
-**#@-*/
-
-include 'salt.php';
--- srcs/requirement/Wordpress/tools/entrypoint.sh
+++ srcs/requirement/Wordpress/tools/entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-if [ ! -d /var/www/wordpress/ ]
-then
-	echo "downloaded wordpress" > /root/manifest
-	wget https://fr.wordpress.org/latest-fr_FR.tar.gz -O - | zcat | tar x -C /var/www/wordpress/
-fi
+cp -r /root/wordpress /var/www/wordpress
 
-cp /root/conf/wp-config.php  /var/www/wordpress/wp-config.php
+#wp core install --allow-root \
+#	--path=/var/www/wordpress --title=ft_wordpress --url=iguidado.42.fr \
+#	--admin_user=Marion --admin_password=Cotillard \
+#	--admin_email=Macotill@nulpar.com --skip-email
 
-if [ ! -f /var/www/wordpress/salt.php ]
-then
-	wget https://api.wordpress.org/secret-key/1.1/salt/ -O /var/www/wordpress/salt.php
-fi
+#if [ ! -d /var/www/wordpress/ ]
+#then
+#	echo "downloaded wordpress" > /root/manifest
+#	wget https://fr.wordpress.org/latest-fr_FR.tar.gz -O - | zcat | tar x -C /var/www/wordpress/
+#fi
+#
+#
+#cp /root/conf/wp-config.php  /var/www/wordpress/wp-config.php
+#
+#if [ ! -f /var/www/wordpress/salt.php ]
+#then
+#	wget https://api.wordpress.org/secret-key/1.1/salt/ -O /var/www/wordpress/salt.php
+#fi
 
 php-fpm8 -F
