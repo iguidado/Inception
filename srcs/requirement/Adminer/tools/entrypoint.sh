99e67456b04be4bc5284628b966c4d60e703dba9
noob3vol <noob.3vol@gmail.com>
2023-04-01 01:21:40 +0200

    fixing wp-config issue

--- srcs/requirement/Adminer/tools/entrypoint.sh
+++ srcs/requirement/Adminer/tools/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /var/www/wordpress/adminer.php ]
 then
-	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -o /var/www/wordpress/adminer.php
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -O /var/www/wordpress/adminer.php
 fi
 
 php-fpm8 -F
--- srcs/requirement/Wordpress/tools/entrypoint.sh
+++ srcs/requirement/Wordpress/tools/entrypoint.sh
@@ -5,27 +5,6 @@ then
 	cp -r /root/wordpress/* /var/www/wordpress
 fi
 
-
-#wp core install --allow-root \
-#	--path=/var/www/wordpress --title=ft_wordpress --url=iguidado.42.fr \
-#	--admin_user=Marion --admin_password=Cotillard \
-#	--admin_email=Macotill@nulpar.com --skip-email
-
-#if [ ! -d /var/www/wordpress/ ]
-#then
-#	echo "downloaded wordpress" > /root/manifest
-#	wget https://fr.wordpress.org/latest-fr_FR.tar.gz -O - | zcat | tar x -C /var/www/wordpress/
-#fi
-#
-#
-#cp /root/conf/wp-config.php  /var/www/wordpress/wp-config.php
-#
-#if [ ! -f /var/www/wordpress/salt.php ]
-#then
-#	wget https://api.wordpress.org/secret-key/1.1/salt/ -O /var/www/wordpress/salt.php
-#fi
-
-
 chown -R nobody:nobody /var/www/wordpress
 
 php-fpm8 -F
