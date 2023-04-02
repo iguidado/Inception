94444e9d1ee20884992e4359417bd9f3acd38d4e
iguidado <iguidado@iguidado.42.fr>
2023-04-02 03:07:46 +0200

    saved bonus from rigged vm

---
+++ srcs/requirement/Bonus/Adminer/tools/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -f /var/www/wordpress/adminer.php ]
+then
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -o /var/www/wordpress/adminer.php
+fi
+
+php-fpm8 -F
