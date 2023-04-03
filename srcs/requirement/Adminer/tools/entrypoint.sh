4bb6436f394651c5d0566febe094b7dab6701782
iguidado <noob3vol@gmail.com>
2023-04-03 22:34:58 +0200

    Fixed redis

--- srcs/requirement/Adminer/tools/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /var/www/wordpress/adminer.php ]
-then
-	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -O /var/www/wordpress/adminer.php
-fi
-
-php-fpm8 -F
