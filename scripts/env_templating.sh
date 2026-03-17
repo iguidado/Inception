3c2bae165328eeed041116e9a78c79ff4c74d231
Nimrod <noob.3vol@gmail.com>
2026-03-17 20:05:26 +0100

    feat: automatised user path configuration in docker volume for automatique deploy

--- scripts/env_templating.sh
+++ scripts/env_templating.sh
@@ -12,19 +12,19 @@ if [ ! -f $1 ]; then
     exit
 fi
 
-
 ADJECTIVES=("happy" "brave" "calm" "eager")
 NAMES=("turing" "lovelace" "knuth" "ritchie")
 
-RANDOM_NAME="${ADJECTIVES[$RANDOM % ${#ADJECTIVES[@]}]}_${NAMES[$RANDOM % ${#NAMES[@]}]}"
-RANDOM_PASS="$(openssl rand -hex 8)"
+
+USERPATH=$HOME #defining home path now because docker daemon run with root account
 
 cp $1 ./srcs/.env
 
+echo "$USERPATH"
+sed "s|PLACEHOLDER_HOME|${USERPATH}|" -i ./srcs/.env #Changed delimiter because path can containt '/'
 for i in {1..2}; do
     RANDOM_NAME="${ADJECTIVES[$RANDOM % ${#ADJECTIVES[@]}]}_${NAMES[$RANDOM % ${#NAMES[@]}]}"
-    RANDOM_PASS="$(openssl rand -hex 8)"
-
+    RANDOM_PASS="$(openssl rand -hex 16)"
 
     sed "s/CHANGE_PASS_$i/${RANDOM_PASS}/" -i ./srcs/.env
     sed "s/CHANGE_NAME_$i/${RANDOM_NAME}/" -i ./srcs/.env
--- setup_docker.sh
+++ setup_docker.sh
@@ -14,6 +14,7 @@ sudo apt-get install -y \
 
 sudo echo "127.0.0.1 ${USER}.42.fr" >> /etc/hosts
 
+
 #Add docker GPG Key (needed to certificate repository)
 sudo groupadd docker
 sudo usermod -aG docker $USER
@@ -21,8 +22,10 @@ sudo usermod -aG docker $USER
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
+
 #Add docker repo to repository
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 
 #Install Docker engine
 sudo apt-get update
@@ -33,10 +36,12 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plu
 sudo usermod -aG docker $(logname)
 sudo chmod 660 /run/docker.sock
 
+
 #Make volume
 ##Wordpress
 sudo mkdir -p /home/$(logname)/data/
 sudo wget https://fr.wordpress.org/latest-fr_FR.tar.gz -O - | zcat | tar x -C /home/$(logname)/data/
+
 
 ##Databases
 #Domain name
